faf866af8fd57aa405052059ad4c2c316aab891c
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-03 23:48:26 +0300

    2020:12:03 23:48

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='django-split-listview',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
